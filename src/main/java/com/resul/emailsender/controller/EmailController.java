72db458ac2491203d30a4d26aabc16eccb69e6a9
resulib <mibadullayev98@gmail.com>
2024-08-15 04:40:56 +0400

    Bulk send option added. upload .txt file send bulk emails

--- src/main/java/com/resul/emailsender/controller/EmailController.java
+++ src/main/java/com/resul/emailsender/controller/EmailController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,10 +16,22 @@ public class EmailController {
     private final EmailService emailService;
 
     @PostMapping("/send")
-    public String sendMail(@RequestParam String to,
-                                           @RequestParam String subject,
-                                           @RequestParam String body) {
-        emailService.sendMail(to, subject, body);
-        return "Email sent successfully!";
+    public String sendEmail(@RequestParam String to,
+                            @RequestParam String subject,
+                            @RequestParam String body) {
+        emailService.sendEmail(to, subject, body);
+        return "E-poçt uğurla göndərildi!";
+    }
+
+    @PostMapping("/sendBulk")
+    public String sendBatchEmails(@RequestParam String subject,
+                                  @RequestParam String body,
+                                  @RequestParam("file") MultipartFile file) {
+        try {
+            int emailCount = emailService.sendBulkEmails(subject, body, file);
+            return emailCount + " istifadəçiyə uğurla E-poçt göndərildi.";
+        } catch (Exception e) {
+            return "Xəta baş verdi: " + e.getMessage();
+        }
     }
 }
--- src/main/java/com/resul/emailsender/service/EmailService.java
+++ src/main/java/com/resul/emailsender/service/EmailService.java
@@ -4,13 +4,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
     private final JavaMailSender mailSender;
 
-    public void sendMail(String to, String subject, String body) {
+    public void sendEmail(String to, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
         message.setSubject(subject);
@@ -19,4 +26,20 @@ public class EmailService {
 
         mailSender.send(message);
     }
+
+    public int sendBulkEmails(String subject, String body, MultipartFile file) throws Exception {
+        Set<String> emailAddresses = new LinkedHashSet<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            emailAddresses.add(line.trim());
+        }
+
+        for (String email : emailAddresses) {
+            sendEmail(email, subject, body);
+        }
+
+        return emailAddresses.size();
+    }
+
 }
